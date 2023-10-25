d404305372800519702c34dd0de588f89cd9d98b
thomas messett <thomas.messett@stampapp.me>
2023-10-25 20:14:05 +0100

    client routes done and associations done

---
+++ app/controllers/clients_controller.rb
@@ -0,0 +1,64 @@
+class ClientsController < ApplicationController
+  before_action :set_client, only: [:show, :edit, :update, :destroy]
+  before_action :set_company, only: [:index, :new, :create]
+  before_action :check_admin_or_company_admin, only: :destroy
+
+  def index
+    @clients = @company.clients
+  end
+
+  def show
+  end
+
+  def new
+    @client = @company.clients.new
+  end
+
+  def create
+    @client = @company.clients.new(client_params)
+    ActiveRecord::Base.transaction do
+      if @client.save
+        ClientAssignment.create!(user: current_user, client: @client)
+        redirect_to [@company, @client], notice: 'Client was successfully created.'
+      else
+        render :new
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @client.update(client_params)
+      redirect_to [@company, @client], notice: 'Client was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @client.destroy
+    redirect_to company_clients_url(@company), notice: 'Client was successfully deleted.'
+  end
+
+  private
+
+  def set_client
+    @client = Client.find(params[:id])
+  end
+
+  def set_company
+    @company = Company.find(params[:company_id])
+  end
+
+  def client_params
+    params.require(:client).permit(:name, :compulsory_fields, :notes, :archived_at)
+  end
+
+  def check_admin_or_company_admin
+    unless current_user.user_type == 'admin' || current_user.is_admin == true
+      redirect_to companies_url, alert: 'You are not authorized to delete this company.'
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   root to: "pages#home"
-
-  resources :companies
-
+  resources :companies do
+    resources :clients
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
